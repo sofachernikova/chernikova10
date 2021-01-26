6f4ff9cfcba19acc0cdca5918a937f6a62d74620
Sonya <sofa.chernikova@mail.ru>
2021-01-26 20:30:45 +0300

    Styles and base JS

---
+++ app.js
@@ -0,0 +1,6 @@
+var area = document.getElementById('area');
+var cell = document.getElementsByClassName('cell');
+var currentPlayer = document.getElementById('curPlyr');
+
+var retryBtn = document.getElementById('retry-btn');
+var resetBtn = document.getElementById('reset-btn');
