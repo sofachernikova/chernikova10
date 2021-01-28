dd1e166994c1877ce88eeca19c03e08e51d133e9
sofachernikova <sofa.chernikova@mail.ru>
2021-01-28 15:26:04 +0300

    Ending canvas block

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ function cellClick() {
 
     if (checkWin(data)) {
         stat[player] += 1;
-        restart("Выграл: " + player);
+        restart("Выиграл: " + player);
     } else {
         var draw = true;
         for (var i in cell) {
@@ -125,5 +125,39 @@ function newRound() {
     //alert('Работает');
     for (var i = 0; i < cell.length; i++) {
         cell[i].innerHTML = '';
+    }
+}
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var retryBtn2 = document.getElementById('retry-btn2');
+
+window.onload = function() {
+    document.getElementById('canvas').onclick = function(event) {
+        console.log(event);
+        ctx.fillStyle = '#4fff5e';
+        ctx.strokeStyle = '#080808';
+        ctx.lineWidth = 6;
+        //левая линия
+        ctx.moveTo(150, 10);
+        ctx.lineTo(10, 140);
+ 
+        //правая линия
+        ctx.moveTo(150, 10);
+        ctx.lineTo(290, 140);
+                
+        //нижняя линия
+        ctx.moveTo(10, 140);
+        ctx.lineTo(290, 140);
+ 
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+        ctx.beginPath();
+    }
+    retryBtn2.onclick = function(event) {
+        console.log(event);
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, 300, 300)
     }
 }
